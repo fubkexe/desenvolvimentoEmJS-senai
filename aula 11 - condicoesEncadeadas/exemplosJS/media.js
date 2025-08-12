23b8306ddabc0cd0cb209af1c63a23efe980863e
Luis Felipe Da Silva Curvello <165702358+fubkexe@users.noreply.github.com>
2025-08-12 16:57:19 -0300

    .

---
+++ aula 11 - condicoesEncadeadas/exemplosJS/media.js
@@ -0,0 +1,14 @@
+let nota1 = 60
+let nota2 = 50
+let media = (nota1 + nota2) / 2
+
+if (media >= 70){
+    console.log(APROVADO);
+    
+} else if (media >=50){
+    console.log("RECUPERAÇÃO");
+    
+} else{
+    console.log(REPROVADO);
+    
+}
