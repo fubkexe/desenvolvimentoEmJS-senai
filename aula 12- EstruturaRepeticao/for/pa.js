5fe187e26845cc62ada62b1dbb4742336c5f42b1
Luis Felipe Da Silva Curvello <165702358+fubkexe@users.noreply.github.com>
2025-08-19 16:59:51 -0300

    .

---
+++ aula 12- EstruturaRepeticao/for/pa.js
@@ -0,0 +1,24 @@
+// Você foi contratado para criar um programa em JavaScript que gere uma Progressão Aritmética (PA).
+// O programa deve pedir ao usuário três informações:
+// O primeiro termo da PA (a1);
+// A razão da PA (r);
+// A quantidade de termos que deseja exibir (n).
+
+//calculando cada termo somando a razão ao termo anterior
+ 
+let termoInicio = 10
+let razao = 2
+let termos = 10
+
+for (let i = 0; i < termos; i++){
+   let termo = termoInicio = termoInicio + razao * i
+    console.log(termoInicio);
+    
+
+    
+
+
+}
+
+
+
---
+++ aula 12- EstruturaRepeticao/html/tabuada/script.js
@@ -0,0 +1,7 @@
+function mostrarResultado(){
+    let numero = parseInt(document.getElementById("numero").value)
+    let resultado = document.getElementById ("resultado")
+
+    for (let i = 0; i <= 10; i++)
+        resultado.innerHTML +=`${numero} x ${i} = ${numero * i} <br> ` 
+}
